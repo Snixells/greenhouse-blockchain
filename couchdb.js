20b899fc261ec633ac3df7122a538e9dff25f2e1
Snixells <bschr9uwb@googlemail.com>
2018-04-11 17:35:10 +0200

    Npm install => body-parser
    Signed-off-by: Snixells <bschr9uwb@googlemail.com>

--- couchdb.js
+++ couchdb.js
@@ -1,5 +1,15 @@
 const NodeCouchDb = require('node-couchdb');
+const bodyParser = require('body-parser');
 
+const couch = new NodeCouchDb({
+    auth: {
+        user: 'Snixells', 
+        password: '5%QO8qSPyXq6#OQF90z*'
+    }
+});
+
+const dbChainName = 'chain';
+const dbUnconfirmedName = 'unconfirmed';
 
 module.exports = {
     
