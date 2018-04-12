ead77c00dd192bb98cc0a59086c9a81174e6162a
Snixells <bschr9uwb@googlemail.com>
2018-04-12 19:21:19 +0200

    Npm install => nano
    Signed-off-by: Snixells <bschr9uwb@googlemail.com>

--- api/routes/unconfirmed_route.js
+++ api/routes/unconfirmed_route.js
@@ -1,8 +1,16 @@
+const Transaction = require('../../Transaction');
+
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
 
+})
+
+router.post('/newTransaction', (req, res, next) => {
+    const transaction = new Transaction({
+        "data" : "data"
+    })  
 })
 
 router.post('/validate', (req, res, next) => {
